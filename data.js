c61ce6dd6da37a24d7f75b03c109bd587f687ac7
Prabhjot Singh <pjsingh1359@gmail.com>
2020-07-06 10:35:55 -0700

    serves the character on click

---
+++ data.js
@@ -0,0 +1,86 @@
+const races = [
+  {
+    "following": "Horde",
+    "name": "Orc",
+    "class": ["Warrior","Hunter","Rogue","Shamen","Mage","Warlock","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Horde",
+    "name": "Undead",
+    "class": ["Warrior","Hunter","Rogue","Mage","Warlock","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Horde",
+    "name": "Tauren",
+    "class": ["Warrior","Hunter","Paladin","Shamen","Priest","Druid","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Horde",
+    "name": "Troll",
+    "class": ["Warrior","Hunter","Rogue","Priest","Shamen","Mage","Warlock","Monk","Druid","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Horde",
+    "name": "Blood Elf",
+    "class": ["Warrior","Hunter","Paladin","Rogue","Priest","Mage","Warlock","Monk","Demon Hunter","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Horde",
+    "name": "Goblin",
+    "class": ["Warrior","Hunter","Rogue","Priest","Shamen","Mage","Warlock","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Horde",
+    "name": "Pandaren",
+    "class": ["Warrior","Hunter","Rogue","Priest","Shamen","Mage","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Alliance",
+    "name": "Pandaren",
+    "class": ["Warrior","Hunter","Rogue","Priest","Shamen","Mage","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Alliance",
+    "name": "Human",
+    "class": ["Warrior","Paladin","Hunter","Rogue","Priest","Mage","Warlock","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Alliance",
+    "name": "Dwarf",
+    "class": ["Warrior","Paladin","Hunter","Rogue","Priest","Shamen","Mage","Warlock","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Alliance",
+    "name": "Night Elf",
+    "class": ["Warrior","Hunter","Rogue","Priest","Mage","Monk","Druid","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Alliance",
+    "name": "Gnome",
+    "class": ["Warrior","Hunter","Rogue","Priest","Mage","Warlock","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Alliance",
+    "name": "Draenei",
+    "class": ["Warrior","Paladin","Hunter","Priest","Shamen","Mage","Monk","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+  {
+    "following": "Alliance",
+    "name": "Worgen",
+    "class": ["Warrior","Hunter","Rogue","Priest","Mage","Warlock","Druid","Death Knight"],
+    "Sex": ["Male", "Female"]
+  },
+];
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const token = 'USEkIkX1v6RnMKYf4ztbWcQrjkDKAcBOpH';
 const raceEl = document.querySelector('.race');
+const genderEl = document.querySelector('.gender');
 const classEl = document.querySelector('.class');
 const allegianceEl = document.querySelector('.allegiance');
+const button = document.querySelector('button');
 
-const raceAPI = `https://us.api.blizzard.com/data/wow/playable-race/index?namespace=static-us&locale=en_US&access_token=${token}`;
+function setRace() {
+    let randomRace = Math.floor(Math.random() * races.length);
+    let randomClass = Math.floor(Math.random() * races[randomRace]['class'].length);
 
-const classesAPI = `https://us.api.blizzard.com/data/wow/playable-class/index?namespace=static-us&locale=en_US&access_token=${token}`;
- 
+    allegianceEl.innerText = races[randomRace]['following'];
+    raceEl.innerText = races[randomRace]['name'];
+    classEl.innerText = races[randomRace]['class'][randomClass];
+};
 
-(async function call() {
-  const classesData = await (await fetch(classesAPI)).json().then(res => res.classes);
-  const racesData = await (await fetch(raceAPI)).json().then(res => res.races.slice(0, 13));
-  const randomNum = Math.floor(Math.random() * classesData.length)
-  console.log(classesData);
-  console.log(racesData);
-})();
+button.addEventListener('click', setRace);
 
-//if anything, write down switch statements that remove classes as options depending on the race. 
+
  
  
 
