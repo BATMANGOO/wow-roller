4a8650bb243fcc198d04801aa6bd4cd8ae580c72
Prabhjot Singh <pjsingh1359@gmail.com>
2020-07-08 15:50:15 -0700

    all core components are in the work

--- data.js
+++ data.js
@@ -2,84 +2,98 @@ const races = [
   {
     "following": "Horde",
     "name": "Orc",
+    "image": 'races/orc.png',
     "class": ["Warrior","Hunter","Rogue","Shamen","Mage","Warlock","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Horde",
     "name": "Undead",
+    "image": 'races/undead.png',
     "class": ["Warrior","Hunter","Rogue","Mage","Warlock","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Horde",
     "name": "Tauren",
+    "image": 'races/tauren.png',
     "class": ["Warrior","Hunter","Paladin","Shamen","Priest","Druid","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Horde",
     "name": "Troll",
+    "image": 'races/troll.png',
     "class": ["Warrior","Hunter","Rogue","Priest","Shamen","Mage","Warlock","Monk","Druid","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Horde",
     "name": "Blood Elf",
+    "image": 'races/blood-elf.png',
     "class": ["Warrior","Hunter","Paladin","Rogue","Priest","Mage","Warlock","Monk","Demon Hunter","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Horde",
     "name": "Goblin",
+    "image": 'races/goblin.png',
     "class": ["Warrior","Hunter","Rogue","Priest","Shamen","Mage","Warlock","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Horde",
     "name": "Pandaren",
+    "image": 'races/pandaren-H.png',
     "class": ["Warrior","Hunter","Rogue","Priest","Shamen","Mage","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Alliance",
     "name": "Pandaren",
+    "image": 'races/pandaren-A.png',
     "class": ["Warrior","Hunter","Rogue","Priest","Shamen","Mage","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Alliance",
     "name": "Human",
+    "image": 'races/human.png',
     "class": ["Warrior","Paladin","Hunter","Rogue","Priest","Mage","Warlock","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Alliance",
     "name": "Dwarf",
+    "image": 'races/dwarf.png',
     "class": ["Warrior","Paladin","Hunter","Rogue","Priest","Shamen","Mage","Warlock","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Alliance",
     "name": "Night Elf",
+    "image": 'races/night-elf.png',
     "class": ["Warrior","Hunter","Rogue","Priest","Mage","Monk","Druid","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Alliance",
     "name": "Gnome",
+    "image": 'races/gnome.png',
     "class": ["Warrior","Hunter","Rogue","Priest","Mage","Warlock","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Alliance",
     "name": "Draenei",
+    "image": 'races/draenei.png',
     "class": ["Warrior","Paladin","Hunter","Priest","Shamen","Mage","Monk","Death Knight"],
     "Sex": ["Male", "Female"]
   },
   {
     "following": "Alliance",
     "name": "Worgen",
+    "image": 'races/worgen.png',
     "class": ["Warrior","Hunter","Rogue","Priest","Mage","Warlock","Druid","Death Knight"],
     "Sex": ["Male", "Female"]
   },
--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 const token = 'USEkIkX1v6RnMKYf4ztbWcQrjkDKAcBOpH';
+const allianceLogo = document.querySelector('.alliance');
+const hordeLogo = document.querySelector('.horde')
 const raceEl = document.querySelector('.race');
+const raceImg = document.querySelector('.raceImage');
 const genderEl = document.querySelector('.gender');
 const classEl = document.querySelector('.class');
-const allegianceEl = document.querySelector('.allegiance');
 const button = document.querySelector('button');
 
 const randomRace = Math.floor(Math.random() * races.length);
 const randomClass = Math.floor(Math.random() * races[randomRace]['class'].length);
 
 (function onStartUp() {
-    allegianceEl.innerText = races[randomRace]['following'];
     raceEl.innerText = races[randomRace]['name'];
     classEl.innerText = races[randomRace]['class'][randomClass];
+    raceImg.setAttribute("src", races[randomRace].image);
+
+    if(races[randomRace].following === 'Horde') {
+        hordeLogo.classList.add('border');
+        allianceLogo.classList.remove('border');
+    } else if(races[randomRace].following === 'Alliance') {
+        allianceLogo.classList.add('border');
+        hordeLogo.classList.remove('border');
+    }
 })()
 
 function setRace() {
+    let hordeBorder = false;
+    let allianceBorder = false;
     let randomRace = Math.floor(Math.random() * races.length);
     let randomClass = Math.floor(Math.random() * races[randomRace]['class'].length);
 
-    allegianceEl.innerText = races[randomRace]['following'];
     raceEl.innerText = races[randomRace]['name'];
     classEl.innerText = races[randomRace]['class'][randomClass];
+    raceImg.setAttribute("src", races[randomRace].image);
+
+    if(races[randomRace].following === 'Horde') {
+        hordeLogo.classList.add('border');
+        allianceLogo.classList.remove('border');
+    } else if(races[randomRace].following === 'Alliance') {
+        allianceLogo.classList.add('border');
+        hordeLogo.classList.remove('border');
+    }
 };
+
+
 
 button.addEventListener('click', setRace);
 
